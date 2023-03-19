9c7ab304c0b695a7df1f9586978c0cf0663fd494
codetype <m15319631269@163.com>
2023-03-19 23:45:10 +0800

    删除配置文件

--- GitLab/config/gitlab.rb
+++
@@ -1,8 +0,0 @@
-gitlab_rails['smtp_enable'] = true
-gitlab_rails['smtp_address'] = "smtp.gmail.com"
-gitlab_rails['smtp_port'] = 587
-gitlab_rails['smtp_user_name'] = "your_email@gmail.com"
-gitlab_rails['smtp_password'] = "your_password"
-gitlab_rails['smtp_authentication'] = "login"
-gitlab_rails['smtp_enable_starttls_auto'] = true
-gitlab_rails['smtp_tls'] = false
