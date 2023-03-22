0884c4daa91c8f4736f1397479c88c49f9ab4f6a
codetype <m15319631269@163.com>
2023-03-22 18:30:55 +0800

    修复docker-compose 安装异常

--- dev.sh
+++ dev.sh
@@ -62,9 +62,8 @@ uninstall_docker(){
 
 # 安装docker-compose
 install_docker-compose() {
-      curl -L https://get.daocloud.io/docker/compose/releases/download/v2.4.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+      wget https://link.storjshare.io/jvczi5rjeusvbksrn7a2jo2a6nya/data%2Fdev%2Fjava%2Fdocker-compose?download=1 -O/usr/local/bin/docker-compose      sudo chmod +x /usr/local/bin/docker-compose
       sudo chmod +x /usr/local/bin/docker-compose
-      sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
       echo "docker-compose安装完成 ！！！"
       exit 1
 }
